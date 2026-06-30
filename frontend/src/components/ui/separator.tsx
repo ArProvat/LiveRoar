5dc8fad0ded8f328315c945c93b51865ac93bed7
ArProvat <provatar0@gmail.com>
2026-06-30 12:37:08 +0600

    feat: define SeparatorProps interface and update Separator component for better type safety

--- frontend/src/components/ui/separator.tsx
+++ frontend/src/components/ui/separator.tsx
@@ -1,17 +1,21 @@
 import * as React from "react";
 import { cn } from "@/lib/utils";
 
+interface SeparatorProps extends React.ComponentProps<"hr"> {
+  orientation?: "horizontal" | "vertical";
+  decorative?: boolean;
+}
+
 function Separator({
   className,
   orientation = "horizontal",
   decorative = true,
   ...props
-}: React.ComponentProps<"hr">) {
+}: SeparatorProps) {
   return (
     <hr
       data-slot="separator"
-      aria-orientation={orientation}
-      decorative={decorative}
+      data-orientation={orientation}
       className={cn(
         "shrink-0 bg-border",
         orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",
